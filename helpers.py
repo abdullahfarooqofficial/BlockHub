781c2767bf0b99371aee94d7eef3f60c47ac1398
abdullahfarooqofficial <abdullahfarooqofficial32@gmail.com>
2026-07-28 15:39:03 +0500

    Wallet API INT..

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from flask import redirect, session
-
+import requests
 
 def login_required(f):
     """
@@ -14,3 +14,19 @@ def login_required(f):
         return f(*args, **kwargs)
 
     return decorated_function
+
+# Wallet API Integration
+
+def get_wallet_data(address, network):
+    """
+    Fetch wallet information from blockchain API.
+    """
+
+    # API integration will be added here later
+
+    return {
+        "address": address,
+        "network": network,
+        "balance": "0.00",
+        "transactions": []
+    }
